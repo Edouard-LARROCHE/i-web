321b5ab79392124150c7fe7b22762c859be8f71f
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-05-19 14:15:08 +0200

    i-web

---
+++ src/components/screen/ScreenClose.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ScreenClose = () => {
+  return (
+    <div>
+      <div className='text-white'>screen</div>
+    </div>
+  );
+};
+
+export default ScreenClose;
--- src/components/start/Phone.jsx
+++ src/components/start/Phone.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import ScreenClose from '../screen/ScreenClose';
 
 const Phone = () => {
   return (
     <div>
-      <div className='h-screen flex justify-center items-center'>
-        <div className='border h-hs w-3/12 rounded-3xl p-1 bg-black'>
+      <div className='h-screen flex justify-center items-center '>
+        <div className='border h-hs w-3/12 rounded-3xl p-1 bg-black overflow-hidden'>
           <div className='border h-full w-full rounded-3xl bg-black2'>
-            <div className='flex justify-center items-center h-16 bg-black2'>
+            <div className='flex justify-center items-center h-16 bg-black2 '>
               <div className='border rounded-full w-1.5 h-1.5' />
               <div className='border w-16 h-2 rounded-lg bg-gray2 ml-ml' />
             </div>
-            <div className='border h-4/5 mx-1 bg-black' />
-            <div className='h-24 bg-black2 flex justify-center items-center'>
+            <div className='border h-4/5 mx-1 bg-black '>
+              <ScreenClose />
+            </div>
+            <div className='h-24 bg-black2 flex justify-center items-center '>
               <div className='border rounded-full w-14 h-14 bg-gray2' />
             </div>
           </div>
