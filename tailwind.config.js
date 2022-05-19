3a59584fb1e645df9b26a9ae30f243df9d76e70c
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-05-19 13:36:36 +0200

    i-web

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,12 @@ module.exports = {
     colors: {
       transparent: 'transparent',
       black: '#000',
+      black1: '#333',
+      black2: '#1F242D',
       white: '#fff',
+      white1: '#e5e5db',
+      white2: '#d4d5cd',
+      white3: '#fcfaf7',
       gray1: 'rgb(17 24 39)',
       gray2: 'rgb(31 41 55)',
     },
