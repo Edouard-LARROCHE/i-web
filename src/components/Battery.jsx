07b000bea554fa0b23cb7e03d96886c1493c0906
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-05-19 15:26:33 +0200

    init battery

---
+++ src/components/Battery.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Battery = () => {
+  return (
+    <div className='flex justify-between mx-2'>
+      <div className=''>I-web</div>
+      <div className=''>75%</div>
+    </div>
+  );
+};
+
+export default Battery;
--- src/components/screen/screenClose/Index.jsx
+++ src/components/screen/screenClose/Index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Battery from '../../Battery';
 import HoursDate from './HoursDate';
 
 const ScreenClose = () => {
   return (
     <div>
       <div className='text-white'>
+        <Battery />
         <HoursDate />
       </div>
     </div>
