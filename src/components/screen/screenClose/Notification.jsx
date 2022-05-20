0013960fb62eef86653ac5da39e03034d533d271
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-05-20 17:38:49 +0200

    notifivcation

--- src/components/screen/screenClose/Notification.jsx
+++ src/components/screen/screenClose/Notification.jsx
@@ -3,14 +3,18 @@ import React from 'react';
 const Notification = () => {
   return (
     <>
-      <div className='flex relative left-5'>
-        <div className='text-white text-xl mr-2'>Centre de notifications</div>
-        <div className=' rounded-full bg-gray2 p-1 w-5 h-5 translate-y-[-10px] cursor-pointer'>
-          <div className='text-white text-xs font-thin translate-y-[-2px] flex justify-center '>X</div>
+      <div className='flex justify-between '>
+        <div className='text-white text-xl mr-2 relative left-5'>Centre de notifications</div>
+        <div className=' rounded-full bg-gray2 p-1 w-6 h-6 cursor-pointer relative right-5'>
+          <div className='text-white text-xs font-thin  flex justify-center '>X</div>
         </div>
       </div>
       <div className='flex relative left-5 mt-3'>
-        <div className='rounded-2xl bg-gray2 w-full h-14 mr-8 opacity-50 cursor-pointer'></div>
+        <div className='rounded-2xl bg-gray2 w-full h-14 mr-8 cursor-pointer flex flex-wrap justify-between'>
+          <div className='ml-3 text-white'>I-web</div>
+          <div className='p-2 text-xs font-thin text-white'>À l'instant</div>
+          <div className='text-white'>BIENVENUE</div>
+        </div>
       </div>
     </>
   );
