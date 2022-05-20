f42806c89165aaaeae831adcdf31423e3dfb50e5
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-05-20 11:45:15 +0200

    animate-open-screen

--- src/components/start/Phone.jsx
+++ src/components/start/Phone.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ScreenClose from '../screen/screenClose/Index';
 import TextToOpen from '../screen/screenClose/TextToOpen';
 
 const Phone = () => {
+  const [openScreen, setOpenScreen] = useState(false);
+
+  const clickToDisplayScreen = () => {
+    setOpenScreen(true);
+  };
+
   return (
     <div>
       <div className='h-screen flex justify-center items-center '>
@@ -12,12 +18,17 @@ const Phone = () => {
               <div className='border rounded-full w-1.5 h-1.5' />
               <div className='border w-16 h-2 rounded-lg bg-gray2 ml-ml' />
             </div>
-            <div className='border h-4/5 mx-1 bg-black '>
-              <ScreenClose />
-              <TextToOpen />
-            </div>
+            {!openScreen ? (
+              <div className='border h-4/5 mx-1 bg-black ' />
+            ) : (
+              <div className='border h-4/5 mx-1 bg-black '>
+                <ScreenClose />
+                <TextToOpen />
+              </div>
+            )}
+
             <div className='h-24 bg-black2 flex justify-center items-center  '>
-              <div className='border rounded-full w-14 h-14 bg-gray2 cursor-grab' />
+              <div className='border rounded-full w-14 h-14 bg-gray2 cursor-grab' onClick={clickToDisplayScreen} />
             </div>
           </div>
         </div>
