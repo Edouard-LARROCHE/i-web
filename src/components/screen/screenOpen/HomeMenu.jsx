7f4bf737afb36f5b8995b796f8e567986a5b9df1
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2022-05-23 17:24:23 +0200

    i-web

--- src/components/screen/screenOpen/HomeMenu.jsx
+++ src/components/screen/screenOpen/HomeMenu.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const HomeMenu = () => {
   return (
     <div className='transition'>
-      <img className='w-20 m-3 cursor-pointer' src='/assets/reglage.png' alt='reglage' />
+      <div className='flex flex-col items-center flex-wrap'>
+        <img className='w-20 m-3 cursor-pointer' src='/assets/reglage.png' alt='reglage' />
+        <div className='text-white text-xs mt-[-8px]'>Réglages</div>
+      </div>
     </div>
   );
 };
